f10cdda914eedb2d1211a76b287b070dd2ddf892
AnnaPersonalDev <annafrias.9@gmail.com>
2025-06-13 14:34:28 +0200

    [feat] add displayError to mock

--- IdealistaiOSChanllenge/IdealistaiOSChanllengeTests/Screens/AdsListTests/Mocks/AdsListViewMock.swift
+++ IdealistaiOSChanllenge/IdealistaiOSChanllengeTests/Screens/AdsListTests/Mocks/AdsListViewMock.swift
@@ -8,11 +8,15 @@
 import XCTest
 @testable import IdealistaiOSChanllenge
 
-
 class AdsListViewMock: AdsListViewControllerProtocol {
     
     var displayAdsCount = 0
+    var displayErrorCount = 0
+
     func displayAds(ads: [AdList]) {
         displayAdsCount += 1
+    }
+    func displayError(error: String) {
+        displayErrorCount += 1
     }
 }
